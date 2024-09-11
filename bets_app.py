6debaac40dc481b4bf718900a37f2d390718c9ee
AlexSchinnerl <alex.itsme@gmx.at>
2024-09-11 10:59:56 +0200

    closed vorab bets

--- bets_app.py
+++ bets_app.py
@@ -5,7 +5,7 @@ st.set_page_config(
     )
 pg = st.navigation([
     st.Page(page="page_help.py", title="Anleitung", url_path="help"),
-    st.Page(page="page_vorab.py", title="Vorab Tipps", url_path="vorab"),
+    # st.Page(page="page_vorab.py", title="Vorab Tipps", url_path="vorab"),
     st.Page(page="page_weekly.py", title="Wöchentliche Tipps", url_path="weekly"),
     st.Page(page="page_standing.py", title="Zwischenstand", url_path="standing"),
     ])
