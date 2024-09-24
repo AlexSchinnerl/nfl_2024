a67cd6bfccebe4e06df147429d8f8d6c6ddb7f97
Alex Schinnerl <alex.itsme@gmx.at>
2024-09-24 15:16:43 +0200

    testing standing display

--- page_standing.py
+++ page_standing.py
@@ -5,7 +5,7 @@ import pandas as pd
 st.header("Zwischenstand")
 
 with st.sidebar:
-    selected_week = st.slider(label="Woche auswählen",value=thisWeek, min_value=1, max_value=18) # value=thisWeek
+    selected_week = st.slider(label="Woche auswählen",value=lastWeek, min_value=1, max_value=18) # value=thisWeek
     y_options = st.multiselect(label="Graph filtern", options=["Gesamtpunkte", "Wöchentliche Punkte"], default=["Gesamtpunkte"])
 
 playerDF = pd.DataFrame(data={"Spieler":player_list}) # sums up 
