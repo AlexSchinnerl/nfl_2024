5b44c1b0420925a1d68761c100a4de01190eb317
Alex Schinnerl <alex.itsme@gmx.at>
2024-09-25 17:47:45 +0200

    tidy up standings

--- page_standing.py
+++ page_standing.py
@@ -2,6 +2,39 @@ import streamlit as st
 from functions_load_and_transform import player_list, schedule, betsDF, scoringDF, lastWeek, thisWeek
 import pandas as pd
 
+st.header("Zwischenstand")
+
+with st.sidebar:
+    selected_week = st.slider(label="Woche auswählen",value=lastWeek, min_value=1, max_value=18)
+
+
+week_view_list = list(range(1, thisWeek))#selected_week+1))
+week_view_list.append("Summe")
+
+scoringDF = scoringDF.drop("Game Nr.", axis=1)
+scoringDF = scoringDF.drop("Winner", axis=1)
+
+weekly_group = scoringDF.groupby("Week").sum()
+week_view = weekly_group.transpose()#
+week_view["Summe"] = week_view.sum(axis=1)
+week_view = week_view[week_view_list]
+week_view.columns = week_view.columns.map(str)
+
+st.dataframe(week_view.sort_values("Summe", ascending=False))
+
+st.subheader("Gesamtpunkte")
+st.bar_chart(week_view, y="Summe", y_label="Gesamtpunkte")
+
+selected_week = st.slider(label="Woche auswählen",value=lastWeek, min_value=1, max_value=18)
+st.subheader(f"Punkte Woche: {selected_week}")
+st.bar_chart(week_view, y=str(selected_week), y_label=f"Punkte", color=(53, 94, 59))
+
+
+
+
+
+# Old Stuff - bei zeiten löschen
+
 # st.header("Zwischenstand")
 
 # with st.sidebar:
@@ -20,27 +53,3 @@ import pandas as pd
 # import streamlit as st
 # from functions_load_and_transform import player_list, schedule, betsDF, scoringDF, lastWeek
 # import pandas as pd
-
-st.header("Zwischenstand")
-
-with st.sidebar:
-    selected_week = st.slider(label="Woche auswählen",value=lastWeek, min_value=1, max_value=18) # value=thisWeek
-    y_options = st.multiselect(label="Graph filtern", options=["Gesamtpunkte", "Wöchentliche Punkte"], default=["Gesamtpunkte"])
-
-week_view_list = list(range(1, selected_week+1))
-week_view_list.append("Summe")
-scoringDF = scoringDF.drop("Game Nr.", axis=1)
-scoringDF = scoringDF.drop("Winner", axis=1)
-# weekly_group = scoringDF.groupby("Week", as_index=False).sum()
-weekly_group = scoringDF.groupby("Week").sum()
-week_view = weekly_group.transpose()#
-week_view["Summe"] = week_view.sum(axis=1)
-week_view = week_view[week_view_list]
-week_view.columns = week_view.columns.map(str)
-
-st.dataframe(week_view)#.sort_values("Summe", ascending=False))
-
-st.subheader("Gesamtpunkte")
-st.bar_chart(week_view, y="Summe", y_label="Gesamtpunkte")
-st.subheader(f"Punkte Woche: {selected_week}")
-st.bar_chart(week_view, y=str(selected_week), y_label=f"Punkte", color=(53, 94, 59))
