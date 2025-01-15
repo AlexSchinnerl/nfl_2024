97cd87ac0d67cc718d5401080dfcfd8cd21dab89
Alex Schinnerl <alex.itsme@gmx.at>
2025-01-15 11:02:31 +0100

    playoff cols

--- page_playoffs.py
+++ page_playoffs.py
@@ -4,13 +4,51 @@ import pandas as pd
 from functions_load_and_transform import player_list, thisWeek, lastWeek, thisWeek_DF, lastWeek_DF
 from functions_form import name_submit, send_form
 
+st.header("Road to Superbowl")
+st.image("PlayoffTree.png")
+
+
+st.header("Playoff Tipps")
+
+colA, colB = st.columns([2,1])
+with colA:
+    st.subheader("Playoff Punktestand")
+
+    playoff_DF = pd.read_csv("data/playoff_view.csv")
+    playoff_DF.set_index("Players", inplace=True)
+
+    st.dataframe(playoff_DF.sort_values("Gesamtpunkte", ascending=False))
+
+with colB:
+    st.subheader("Hier Tipps auswählen")
+    with st.form("Place Bet"):
+        selected_teams = []
+        for pairing in zip(thisWeek_DF["Home Team"], thisWeek_DF["Away Team"], thisWeek_DF["Game Nr."]):
+            chosen_winner = st.selectbox(
+                label=f"Game Nr.: {pairing[2]} {pairing[0]} vs. {pairing[1]}", 
+                options=pairing[0:2], 
+                index=None, 
+                placeholder="Bitte Gewinner auswählen"
+                )
+            selected_teams.append(chosen_winner)
+        player_name, weekly_submitted = name_submit("Tipps absenden")
+    if weekly_submitted:
+        selected_teams.append(player_name)
+        send_form(mailText=selected_teams[:-1], subject=f"bets_{selected_teams[-1]}_week_{thisWeek}")
+
+col_A, col_B = st.columns([2,1])
+with col_A:
+    st.subheader(f"Ergebnisse für Woche {lastWeek}")
+    st.dataframe(lastWeek_DF, hide_index=True, height=600)
+with col_B:
+    st.subheader(f"Spielplan für Woche {thisWeek}")
+    st.dataframe(thisWeek_DF, hide_index=True, height=600)
+
+
 # from streamlit_flow import streamlit_flow
 # from streamlit_flow.elements import StreamlitFlowNode, StreamlitFlowEdge 
 # from streamlit_flow.state import StreamlitFlowState
 # from streamlit_flow.layouts import TreeLayout
-
-st.header("Road to Superbowl")
-st.image("PlayoffTree.png")
 
 # nodes = [
 #     StreamlitFlowNode(id='1', pos=(66, 0), data={'content': 'Chiefs'}, node_type='input', source_position='right'),
@@ -63,45 +101,3 @@ st.image("PlayoffTree.png")
 #                 show_controls=True,
 #                 pan_on_drag=False,
 #                 allow_zoom=True)
-
-
-
-
-st.header("Playoff Scores")
-
-playoff_DF = pd.read_csv("data/playoff_view.csv")
-playoff_DF.set_index("Players", inplace=True)
-
-st.dataframe(playoff_DF.sort_values("Gesamtpunkte", ascending=False))
-
-
-st.header("Playoff Tipps")
-
-colA, colB = st.columns([2,1])
-with colA:
-    if thisWeek == 1:
-        pass
-    else:
-        st.subheader(f"Ergebnisse für Woche {lastWeek}")
-        st.dataframe(lastWeek_DF, hide_index=True, height=600)
-
-    st.subheader(f"Spielplan für Woche {thisWeek}")
-    st.dataframe(thisWeek_DF, hide_index=True, height=600)
-
-with colB:
-    st.subheader("Hier Tipps auswählen")
-    with st.form("Place Bet"):
-        selected_teams = []
-        for pairing in zip(thisWeek_DF["Home Team"], thisWeek_DF["Away Team"], thisWeek_DF["Game Nr."]):
-            chosen_winner = st.selectbox(
-                label=f"Game Nr.: {pairing[2]} {pairing[0]} vs. {pairing[1]}", 
-                options=pairing[0:2], 
-                index=None, 
-                placeholder="Bitte Gewinner auswählen"
-                )
-            selected_teams.append(chosen_winner)
-        player_name, weekly_submitted = name_submit("Tipps absenden")
-    if weekly_submitted:
-        selected_teams.append(player_name)
-        send_form(mailText=selected_teams[:-1], subject=f"bets_{selected_teams[-1]}_week_{thisWeek}")
-
